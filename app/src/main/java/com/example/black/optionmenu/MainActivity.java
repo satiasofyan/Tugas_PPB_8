85717e681e5f5831e6819f88b418e583d9964f41
Agus Ramdan <1406009@sttgarut.ac.id>
2017-12-03 14:28:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/black/optionmenu/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.black.optionmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String [] isi = {"Baso Yamin","Mie Ayam","Baso Beranak","Baso Tulang","Baso Kumplit","Baso Urat"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.list_view,isi);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        registerForContextMenu(listView);
+    }
+    public void onCreateContextMenu(ContextMenu menu , View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu,v,menuInfo);
+        menu.add(0,v.getId(),0,"Bold");
+        menu.add(0,v.getId(),0,"Italic");
+        menu.add(0,v.getId(),0,"Normal");
+    }
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getTitle() == "Bold") {
+            Toast.makeText(getApplicationContext(), "Memilih Bold", Toast.LENGTH_SHORT).show();
+        } else if (item.getTitle() == "Italic") {
+            Toast.makeText(getApplicationContext(), "Memilih Italic", Toast.LENGTH_SHORT).show();
+        } else if (item.getTitle() == "Normal") {
+            Toast.makeText(getApplicationContext(), "Memilih Normal", Toast.LENGTH_SHORT).show();
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected( MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.bookmark:
+                Toast.makeText(getApplicationContext(), "Bookmark dipilih", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.search:
+                Toast.makeText(getApplicationContext(), "Search dipilih", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.share:
+                Toast.makeText(getApplicationContext(), "share dipilih", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.delete:
+                Toast.makeText(getApplicationContext(), "delete dipilih", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.preferences:
+                Toast.makeText(getApplicationContext(), "Preferences dipilih", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.save:
+                Toast.makeText(getApplicationContext(), "save dipilih", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
+
